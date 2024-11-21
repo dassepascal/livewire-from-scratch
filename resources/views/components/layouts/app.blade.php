dfc45b68af34eee082e66846569452820fe59c02
Dasse <pascaldasse56@gmail.com>
2024-11-21 21:24:57 +0100

    episode 10

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -16,7 +16,7 @@
 </head>
 
 <body>
-    <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
+    <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50" x-data x-on:click="$dispatch('search:clear-results')">
    
         <div class="relative flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
             <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
@@ -35,6 +35,14 @@
             </div>
         </div>
     </div>
+<script data-navigate-once>
+//     document.addEventListener('search:clear-results', function(e)
+// {
+//     console.log('cleared results');
+// })
+console.log('page loaded');
+
+    </script>
 
 
 
