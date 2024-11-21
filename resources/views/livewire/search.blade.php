6dbd984010de287cb0a8c3e2976980f74e1a0992
Dasse <pascaldasse56@gmail.com>
2024-11-21 18:02:05 +0100

    episode 8

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -7,9 +7,8 @@
                 type="text"
                 class=" p-4 w-9/12 border rounded-md bg-gray-700 text-white"
                 wire:model.live.debounce="searchText"
-                placeholder="Search..."                 
+                placeholder="{{$placeholder}}"                 
                 >
-
                 <button class="text-white font-medium rounded-md p-4 disabled:bg-indigo-400 bg-indigo-600"
                 wire:click.prevent = "clear()"
 
@@ -24,13 +23,5 @@
           
         
     </form>
-    <div class="mt-4">
-        @foreach ($this->results as $result   )
-            <div class="pt-2">
-              <a href="/articles/{{$result->id}}"> {{$result->title}} </a>
-            </div>
-          
-        @endforeach
-    </div>
-   
+    <livewire:search-results :results="$results" :show="!empty($searchText)"/>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/search',Search::class)->name('search');
+//Route::get('/search',Search::class)->name('search');
 Route::get('/articles/{article}',\App\Livewire\ShowArticle::class)->name('show.article');
 
 
